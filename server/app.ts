3a2782c23bb355943fa56504beb2ca2c9092eff6
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-09-12 23:19:58 -0700

    server initiated

---
+++ server/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = 8000
+
+app.get('/', (req, res) => {
+  res.send('Express + TS Server')
+  console.log(req)
+})
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
