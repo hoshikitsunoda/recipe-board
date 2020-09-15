9ff83144fb0df5358758f6cc672e69fa43fcb0dc
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-09-14 22:19:11 -0700

    setup apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import RecipeList from './components/RecipeList'
+
+const URI = `http://localhost:8000/graphql`
+
+const client = new ApolloClient({
+  uri: URI,
+  cache: new InMemoryCache(),
+})
 
 const App = () => {
-  return <div className="App">Client Side</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Client Side</h2>
+        <RecipeList />
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App
