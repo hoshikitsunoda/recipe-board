2adc6187673b0ac7f50efe45be6b91ab1e0780f7
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-12-05 23:52:42 -0800

    updated styling

--- client/src/components/RecipeListView/components/RecipeItem.tsx
+++ client/src/components/RecipeListView/components/RecipeItem.tsx
@@ -12,39 +12,49 @@ const RecipeItem: React.FC<IRecipe> = ({
   instructions,
 }) => {
   return (
-    <div className="w-full max-w-sm overflow-scroll px-6 py-4 bg-orange-100 flex flex-col justify-between shadow-solidPink">
-      <h2 className="font-mono text-gray-800 text-lg">{recipe}</h2>
-      <ul className="font-serif text-base mt-4">
-        <p className="font-mono text-xs mb-2">Ingredients:</p>
-        {ingredients?.map(({ ingredient, quantity, unit }: IIngredient) =>
-          ingredient ? (
-            <li key={ingredient} className="tracking-wide">
-              {ingredient} - {quantity} {unit}
-            </li>
-          ) : null
-        )}
-      </ul>
-      <p className="font-serif text-sm mt-6 tracking-wide">{instructions}</p>
-      <LinkButton id={id}>See the recipe</LinkButton>
-      <Mutation
-        mutation={REMOVE_RECIPE}
-        variables={{ id: id }}
-        refetchQueries={() => {
-          return [
-            {
-              query: RECIPES_QUERY,
-            },
-          ]
-        }}
-      >
-        {(mutation: any) => (
-          <div className="w-full text-right mt-4">
-            <Button onClick={mutation} red>
-              delete
-            </Button>
-          </div>
-        )}
-      </Mutation>
+    <div className="w-full max-w-sm relative shadow-solidPink overflow-scroll flex flex-col justify-between">
+      <div className="h-full px-6 py-4 bg-orange-100 flex flex-col justify-between">
+        <div>
+          <h2 className="font-mono text-gray-800 text-lg">{recipe}</h2>
+          <ul className="font-serif text-base mt-4">
+            <p className="font-mono text-xs mb-2">Ingredients:</p>
+            {ingredients?.map(({ ingredient, quantity, unit }: IIngredient) =>
+              ingredient ? (
+                <li key={ingredient} className="tracking-wide">
+                  {ingredient} - {quantity} {unit}
+                </li>
+              ) : null
+            )}
+          </ul>
+          <p className="font-serif text-sm mt-6 tracking-wide">
+            {instructions}
+          </p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="w-9/12">
+          <LinkButton id={id}>See the recipe</LinkButton>
+        </div>
+        <Mutation
+          mutation={REMOVE_RECIPE}
+          variables={{ id: id }}
+          refetchQueries={() => {
+            return [
+              {
+                query: RECIPES_QUERY,
+              },
+            ]
+          }}
+        >
+          {(mutation: any) => (
+            <div className="w-3/12">
+              <Button onClick={mutation} red>
+                delete
+              </Button>
+            </div>
+          )}
+        </Mutation>
+      </div>
     </div>
   )
 }
